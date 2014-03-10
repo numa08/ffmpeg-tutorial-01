df2df9a5aac4086ef021bab7fae1f11b418e6847
numa08 <n511287@gmail.com>
2014-03-11 02:21:04 +0900

    動画を格納する感じのディレクトリを作った

--- app/src/main/java/net/numa08/android_ffmpeg_tutorial_01/app/MainActivity.java
+++ app/src/main/java/net/numa08/android_ffmpeg_tutorial_01/app/MainActivity.java
@@ -7,13 +7,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.File;
+
 
 public class MainActivity extends Activity {
+    public static final String FRAME_DUMP_DIR = "android-ffmpeg-tutorial-01";
     public static final int MAX_FRAME_OF_NUM = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final File dumpDir = new File(getFilesDir().getPath() + File.separator + MainActivity.FRAME_DUMP_DIR);
+        if (!dumpDir.exists()) {
+            dumpDir.mkdir();
+        }
     }
 
     public void clickStartButton(View view) {
