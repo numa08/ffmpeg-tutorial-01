1f4e891944fe2bb7c594d744ff425e68720e645f
numa08 <n511287@gmail.com>
2014-03-12 01:05:41 +0900

    ファイルのコピーはうまく行っている

--- app/src/main/java/net/numa08/android_ffmpeg_tutorial_01/app/MainActivity.java
+++ app/src/main/java/net/numa08/android_ffmpeg_tutorial_01/app/MainActivity.java
@@ -1,26 +1,32 @@
 package net.numa08.android_ffmpeg_tutorial_01.app;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import java.io.File;
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements DumpFrameTask.DumFrameTaskLisner{
     public static final String TARGET_MOVIE_FILE = "1.mp4";
     public static final String FRAME_DUMP_DIR = "android-ffmpeg-tutorial-01";
     public static final int MAX_FRAME_OF_NUM = 20;
+
+    private Dialog mProgressDialog;
+    private DumpFrameTask mDumpFrameTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final File dumpDir = new File(getFilesDir().getPath() + File.separator + MainActivity.FRAME_DUMP_DIR);
+        final File dumpDir = new File(movieFileDir());
         if (!dumpDir.exists()) {
             dumpDir.mkdir();
         }
@@ -30,6 +36,7 @@ public class MainActivity extends Activity {
             Log.e("FFMpeg Tutorial", "cannot copy target file");
         }
     }
+
 
     public void clickStartButton(View view) {
         final EditText numberEditText = (EditText)findViewById(R.id.editText);
@@ -45,5 +52,49 @@ public class MainActivity extends Activity {
         if (numOfFrame > MainActivity.MAX_FRAME_OF_NUM) {
             numOfFrame = MainActivity.MAX_FRAME_OF_NUM;
         }
+        final File dumDir = new File(getFilesDir(), "dump");
+        if (dumDir.exists()) {
+            dumDir.delete();
+        }
+        dumDir.mkdir();
+        final String dumedDirPath = dumDir.getPath();
+        final String frameVideoPath = movieFileDir() + File.separator + MainActivity.TARGET_MOVIE_FILE;
+        final FrameDumper dumper = new FrameDumper(dumedDirPath, frameVideoPath, numOfFrame);
+        mDumpFrameTask = new DumpFrameTask(dumper);
+//        mDumpFrameTask.execute();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mDumpFrameTask != null) {
+            mDumpFrameTask.cancel(false);
+        }
+    }
+
+    @Override
+    public void onPreExecute() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+
+        mProgressDialog = ProgressDialog.show(this, "Dum Frame", "Please wait", false);
+    }
+
+    @Override
+    public void onPostExecute(FrameDumper dumper) {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        final List<String> dumedFiles = dumper.dumpedFilePathes();
+        final PagerAdapter adapter = new VideoFrameAdapter(dumedFiles);
+        viewPager.setAdapter(adapter);
+    }
+
+    private String movieFileDir() {
+        return new StringBuffer(getFilesDir().getPath())
+                   .append(File.separator)
+                   .append(MainActivity.FRAME_DUMP_DIR).toString();
     }
 }
